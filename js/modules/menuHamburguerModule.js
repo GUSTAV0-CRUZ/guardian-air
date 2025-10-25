87e16a9f4c4bbdfecaa04592121e9503e9342260
Gustavo Cruz <gustavocruzsantos39@gmail.com>
2025-10-25 02:03:07 -0300

    Revert "Merge pull request #4 from GUSTAV0-CRUZ/feature/acessibilidade"
    This reverts commit ec17c864165d9cadec2118576282cad6d3d12218, reversing
    changes made to 02bc9fc521352723bcdb6201d6e97b1fbbd8f323.

--- js/modules/menuHamburguerModule.js
+++ js/modules/menuHamburguerModule.js
@@ -5,15 +5,10 @@ const menuHamburguer = document.createElement('button');
 menuHamburguer.classList.add('menu-hamburger');
 menuHamburguer.innerHTML = '&#9776;';
 menuHamburguer.setAttribute('aria-label', 'Abrir menu de navegação');
-menuHamburguer.setAttribute('aria-expanded', 'false');
-menuHamburguer.setAttribute('aria-controls', 'nav-menu');
 navBar.appendChild(menuHamburguer);
 
 export function mostraMenu() {
     menuHamburguer.addEventListener('click', e => {
         navList.classList.toggle('active');
-        menuHamburguer.setAttribute('aria-expanded', navList.classList.contains('active')); 
-        if(navList.classList.contains('active')) return menuHamburguer.setAttribute('aria-label', 'Fechar menu de navegação');
-        menuHamburguer.setAttribute('aria-label', 'Abrir menu de navegação');
     })
 }
