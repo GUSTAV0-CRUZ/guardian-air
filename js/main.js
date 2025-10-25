132d9993abd0e0c0addfd9784798298dc83871a7
Gustavo Cruz <gustavocruzsantos39@gmail.com>
2025-10-25 00:20:34 -0300

    feat(acessibilidade): implementa modo noturno, contraste e navegação por teclado

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ import Form from "./modules/validaformModule.js";
 import { saibaMais } from "./modules/saibamaisModule.js";
 import { eventos } from "./modules/doaocoesModule.js";
 import { mostraMenu } from "./modules/menuHamburguerModule.js";
+import { ModoEscuro } from "./modules/modoEscuroModule.js";
 
 const form = new Form(document.querySelector('form'))
+const modoEscuro = new ModoEscuro(localStorage.getItem('tema'))
 
+modoEscuro.inicia();
 form.inicia();
 saibaMais();
 mostraMenu();
