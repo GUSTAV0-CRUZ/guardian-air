2f797fb1c8ce381f8900774c3724ac9ab4655bb3
Gustavo Cruz <gustavocruzsantos39@gmail.com>
2025-10-25 23:24:27 -0300

    hotfix(js): corrige erro de 'null' no load com DOMContentLoaded

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ import { eventos } from "./modules/doaocoesModule.js";
 import { mostraMenu } from "./modules/menuHamburguerModule.js";
 import { ModoEscuro } from "./modules/modoEscuroModule.js";
 
-const form = new Form(document.querySelector('form'))
-const modoEscuro = new ModoEscuro(localStorage.getItem('tema'))
-
-modoEscuro.inicia();
-form.inicia();
-saibaMais();
-mostraMenu();
-
-if(document.querySelector('#valorDoacaoEscolha')) eventos();
+document.addEventListener('DOMContentLoaded', () => {
+    const form = new Form(document.querySelector('form'))
+    const modoEscuro = new ModoEscuro(localStorage.getItem('tema'))
+    
+    modoEscuro.inicia();
+    form.inicia();
+    saibaMais();
+    mostraMenu();
+    
+    if(document.querySelector('#valorDoacaoEscolha')) eventos();
+});
